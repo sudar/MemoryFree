45bd79ac026da338b863ebc346b9175d1daa339e
Sudar <sudar@sudarmuthu.com>
2012-08-05 17:36:16 +0530

    Initial Commit

---
+++ MemoryFree.c
@@ -0,0 +1,56 @@
+/**
+ * MemoryFree - Arduino library to find out the amount of free memory available.
+ *
+ * Includes code from the following places
+ *
+ * Original code from - http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1213583720/19#19
+ * Minimum RAM Check from - http://github.com/ribbotson/USB-Host/tree/master/Memory_Free/
+ *
+ */
+
+#include "MemoryFree.h"
+
+// predefined variables
+extern unsigned int __data_start;
+extern unsigned int __data_end;
+extern unsigned int __bss_start;
+extern unsigned int __bss_end;
+extern unsigned int __heap_start;
+extern void *__brkval;
+
+int memory_min = 0x7FFF; // default bigger value
+
+/**
+ * Get the total free memory 
+ */
+int getFreeMemory()
+{
+    int free_memory;
+
+    if((int)__brkval == 0)
+        free_memory = ((int)&free_memory) - ((int)&__bss_end);
+    else
+        free_memory = ((int)&free_memory) - ((int)__brkval);
+
+    return free_memory;
+}
+
+/**
+ * Checks for the new minimum memory
+ */
+void memoryCheck()
+{
+    int free_memory = getFreeMemory();
+
+    if (free_memory < memory_min) memory_min = free_memory;
+
+    return;
+}
+
+/**
+ * Get the minimum memory
+ */
+int getMinMemory()
+{
+    return memory_min;
+}
---
+++ MemoryFree.h
@@ -0,0 +1,37 @@
+/**
+ * MemoryFree - Arduino library to find out the amount of free memory available.
+ *
+ * Includes code from the following places
+ *
+ * Original code from - http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1213583720/19#19
+ * Minimum RAM Check from - http://github.com/ribbotson/USB-Host/tree/master/Memory_Free/
+ *
+ */
+
+#ifndef	MEMORY_FREE_H
+#define MEMORY_FREE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Get the total free memory 
+ */
+int getFreeMemory();
+
+/**
+ * Checks for the new minimum memory
+ */
+void memoryCheck();
+
+/**
+ * Get the minimum memory
+ */
+int getMinMemory();
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif
